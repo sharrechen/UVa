85c90aecdd29eb8960cbd1a0d733308a112c7662
Sharon Chen <dodophiphy@gmail.com>
2015-01-01 01:36:08 +0800

    use array to record history

--- 100 - The 3n + 1 problem.cpp
+++ 100 - The 3n + 1 problem.cpp
@@ -7,25 +7,29 @@ int main(void) {
 
 	int a, b, begin, end, maxclen, clength;
 	unsigned long long n;
-
+	int history[1000000] = {};
 	while (scanf("%d %d", &a, &b)!= EOF) {
 		begin = a;
 		end = b;
 		if (end < begin) swap(begin, end);
 		maxclen = 0;
 		for (int i = begin; i <= end; i++) {
-			clength = 1;
-			n = i;
-			while (n != 1) {
-				if (n & 1) {
-					n = 3 * n + 1;
-				}else{
-					n >>= 1;
+			if (history[i]) {
+				maxclen = max( maxclen, history[i]);
+			} else {
+				clength = 1;
+				n = i;
+				while (n != 1) {
+					if (n & 1) {
+						n = 3 * n + 1;
+					}else{
+						n >>= 1;
+					}
+					clength++;
 				}
-				clength++;
+				history[i] = clength;
+				maxclen = max( maxclen, clength);
 			}
-			maxclen = max( maxclen, clength);
-			
 		}
 		printf("%d %d %d\n", a, b, maxclen); 
 	}
