e55d8a1b5fbf17a2b42db97687e5f4cbfe995a1f
Sharon Chen <dodophiphy@gmail.com>
2015-01-01 01:13:52 +0800

    simplify

--- 100 - The 3n + 1 problem.cpp
+++ 100 - The 3n + 1 problem.cpp
@@ -1,35 +1,31 @@
 #include <iostream>
+#include <algorithm>
 #include <cstdlib>
 using namespace std;
 
-int n3_1(unsigned long long n) {
-	int clength = 1;
-	while (n != 1) {
-		if (n & 1) {
-			n = 3 * n + 1;
-		}else{
-			n >>= 1;
-		}
-		clength++;
-	}
-	return clength;
-}
-
 int main(void) {
 
-	int a, b, begin, end, maxclen;
-	int history[1000000]={};
+	int a, b, begin, end, maxclen, clength;
+	unsigned long long n;
 
 	while (cin >> a >> b) {
-		begin = a < b ? a : b;
-		end = a > b ? a : b;
+		begin = a;
+		end = b;
+		if (end < begin) swap(begin, end);
 		maxclen = 0;
 		for (int i = begin; i <= end; i++) {
-			if (maxclen < history[i] ) {
-				maxclen = history[i];
-			}else if (maxclen < (history[i] = n3_1(i))) {
-				maxclen = history[i];
+			clength = 1;
+			n = i;
+			while (n != 1) {
+				if (n & 1) {
+					n = 3 * n + 1;
+				}else{
+					n >>= 1;
+				}
+				clength++;
 			}
+			maxclen = max( maxclen, clength);
+			
 		}
 		cout << a << " " << b << " " << maxclen <<endl; 
 	}
